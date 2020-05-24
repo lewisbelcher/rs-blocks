1e2e60dda3696cff371fb6a3bb879ece2daf3970
Lewis Belcher <belcher_lewis@protonmail.com>
2020-05-24 19:23:44 +0200

    Test checking what gitlab thinks the token is

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 # Publish crates on crates.io
 
 set -e
+echo $CARGO_TOKEN
 cargo login $CARGO_TOKEN
 cd rs-blocks-derive
 cargo publish || :  # Allow error if version exists (other errors should be caught below)
